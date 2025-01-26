b7de67b8e85ddad65dc51cfa07509def1f4d3aec
Adrian <adriansanduta@gmail.com>
2025-01-25 18:35:26 -0600

    1.3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import React from 'react';
 import './App.css';
 import { auth } from './firebase/init'; 
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut} from "firebase/auth";
 
 function App() {
+  const [user, setUser] = React.useState({});
   function register() {
     console.log('register');
     createUserWithEmailAndPassword(auth, 'email@email.com', 'test123')
@@ -34,10 +36,15 @@ function App() {
     });
   }
 
+  function logout() {
+    auth.signOut();
+  }
+
   return (
     <div className="App">
       <button onClick={register}>Register</button>
       <button onClick={login}>Login</button>
+      <button onClick={logout}>Logout</button>
     </div>
   );
 }
