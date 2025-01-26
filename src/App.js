caff3fbdbbe36ad5601174625fa678f6fde138f7
Adrian <adriansanduta@gmail.com>
2025-01-25 18:31:13 -0600

    1.2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import {auth}  from './firebase/init'; 
-import {  createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from './firebase/init'; 
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 function App() {
   function register() {
@@ -18,10 +18,26 @@ function App() {
       console.log(errorCode, errorMessage);
     });
   }
+
+  function login() {
+    signInWithEmailAndPassword(auth, 'email@email.com', 'test123')
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log(user);
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+    });
+  }
+
   return (
     <div className="App">
       <button onClick={register}>Register</button>
-
+      <button onClick={login}>Login</button>
     </div>
   );
 }
