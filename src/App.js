b502a583ef9dc4b9e6f6837c9b08d063f4b889c4
Adrian <adriansanduta@gmail.com>
2025-01-25 18:19:59 -0600

    1.1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {auth}  from './firebase/init'; 
+import {  createUserWithEmailAndPassword } from "firebase/auth";
 
 function App() {
+  function register() {
+    console.log('register');
+    createUserWithEmailAndPassword(auth, 'email@email.com', 'test123')
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      console.log(user);
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={register}>Register</button>
+
     </div>
   );
 }
